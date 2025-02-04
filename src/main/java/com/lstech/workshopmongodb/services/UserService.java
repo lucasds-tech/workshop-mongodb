0eda2f218186f5387986a59e08ff0e9213997be3
Lucas Silva <lucas.dasilva09@hotmail.com>
2025-02-04 16:44:01 -0300

    Getting user by id

--- src/main/java/com/lstech/workshopmongodb/services/UserService.java
+++ src/main/java/com/lstech/workshopmongodb/services/UserService.java
@@ -1,11 +1,13 @@
 package com.lstech.workshopmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lstech.workshopmongodb.domain.User;
+import com.lstech.workshopmongodb.exception.ObjectNotFoundException;
 import com.lstech.workshopmongodb.repository.UserRepository;
 
 @Service
@@ -17,4 +19,10 @@ public class UserService {
 	public List<User> findAll() {
 		return repo.findAll();
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
 }
