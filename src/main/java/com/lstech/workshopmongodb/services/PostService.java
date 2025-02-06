423d9c3496b1a267f83385ee67c2cc119fa09b53
Lucas Silva <lucas.dasilva09@hotmail.com>
2025-02-06 12:24:45 -0300

    Getting a post by id

---
+++ src/main/java/com/lstech/workshopmongodb/services/PostService.java
@@ -0,0 +1,22 @@
+package com.lstech.workshopmongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lstech.workshopmongodb.domain.Post;
+import com.lstech.workshopmongodb.exception.ObjectNotFoundException;
+import com.lstech.workshopmongodb.repository.PostRepository;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
