971e4afd66b8bbac6482fe3fa65c263d6676beec
Lucas Silva <lucas.dasilva09@hotmail.com>
2025-02-06 13:11:27 -0300

    Implementing comments on posts

--- src/main/java/com/lstech/workshopmongodb/config/Instantiation.java
+++ src/main/java/com/lstech/workshopmongodb/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.lstech.workshopmongodb.domain.Post;
 import com.lstech.workshopmongodb.domain.User;
 import com.lstech.workshopmongodb.dto.AuthorDTO;
+import com.lstech.workshopmongodb.dto.CommentDTO;
 import com.lstech.workshopmongodb.repository.PostRepository;
 import com.lstech.workshopmongodb.repository.UserRepository;
 
@@ -41,6 +42,13 @@ public class Instantiation implements CommandLineRunner {
 		
 		Post post1 = new Post(null, sdf.parse("21/01/2025"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(carlos));
 		Post post2 = new Post(null, sdf.parse("23/01/2025"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(carlos));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem cara!", sdf.parse("21/01/2025"), new AuthorDTO(lucas));
+		CommentDTO c2 = new CommentDTO("Aproveite bem!", sdf.parse("22/01/2025"), new AuthorDTO(diogenes));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/01/2025"), new AuthorDTO(lucas));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
