48b88f08edfd321e347ebbcb5959e1a7f8d2d8ec
Lucas Silva <lucas.dasilva09@hotmail.com>
2025-02-10 18:08:38 -0300

    Update

--- src/main/java/com/lstech/workshopmongodb/repository/PostRepository.java
+++ src/main/java/com/lstech/workshopmongodb/repository/PostRepository.java
@@ -14,6 +14,6 @@ public interface PostRepository extends MongoRepository<Post, String> {
 
 	List<Post> findByTittleContainingIgnoreCase(String text);
 	
-	@Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	@Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'tittle': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
 	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
